61d0d7a86701ad80db122ac4df0b183dfb822566
Sathishkumar <sathish.ipunch@gmail.com>
2023-06-11 12:17:07 +0530

    Episode-02

--- App.js
+++ App.js
@@ -1,26 +1,11 @@
-// const heading = React.createElement("h1",
-// {
-//     id:"heading",
-//     custom:"title"
-// }
-// ,"Hello world From React JS");
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(heading);
-
-{/* <div id="parent">
-    <div id="child">
-        <h1>I'm H1 tag</h1>
-    </div>
-</div> */}
-
+import React from "react";
+import ReactDOM  from "react-dom/client";
 const parent = React.createElement(
     "div",{id:"parent"},
    [ React.createElement("div",{id:"child"}
    ,[React.createElement("h1",{},"I'm h1 tag"
    ),React.createElement("h2",{},"I'm h2 tag"
-   )]), React.createElement("div",{id:"child2"}
+   )]), React.createElement("div",{id:"child2 "}
    ,[React.createElement("h1",{},"I'm h1 tag"
    ),React.createElement("h2",{},"I'm h2 tag"
    )])]
